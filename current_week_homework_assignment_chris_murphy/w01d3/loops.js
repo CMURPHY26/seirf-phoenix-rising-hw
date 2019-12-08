055ffad34d013328a2c4c7fa1143a99b4ff0f53a
Chris <cl@Chriss-MBP.fios-router.home>
2019-12-08 18:07:39 -0500

    finished loop practice

---
+++ current_week_homework_assignment_chris_murphy/w01d3/loops.js
@@ -0,0 +1,35 @@
+for (let i = 0; i < 100; i++) {
+    console.log(i)
+}
+
+for (let i = 99; i >= 0; i--) {
+    console.log(i)
+}
+
+for (let i = 0; i < 100; i += 2) {
+    console.log(i)
+}
+
+for (let i = 98; i >= 0; i -= 2) {
+    console.log(i)
+}
+
+for (let i = 1; i < 100; i += 2) {
+    console.log(i)
+}
+
+for (let i = 99; i >= 0; i -= 2) {
+    console.log(i)
+}
+
+for (let i = 49; i <= 72; i++){
+    console.log(i);
+}
+
+for (let i = 81; i >= 26; i--){
+    console.log(i);
+}
+
+for (let i = 3; i <= 90; i += 3){
+    console.log(i);
+}
