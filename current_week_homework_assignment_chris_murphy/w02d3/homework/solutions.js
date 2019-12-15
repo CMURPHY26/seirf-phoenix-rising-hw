6ba893b45778e7a74b5108d81213940e22d418be
Chris <cl@Chriss-MBP.fios-router.home>
2019-12-15 13:59:35 -0500

    rock paper scissors completed

--- current_week_homework_assignment_chris_murphy/w02d3/homework/solutions.js
+++ current_week_homework_assignment_chris_murphy/w02d3/homework/solutions.js
@@ -51,27 +51,64 @@
 // PRIME NUMBERS
 ////////////////////////
 
-const checkPrime = (num) => {
-    for(let i = 2; i <= Math.sqrt(num); i++) {
-        console.log(i);
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return true
-}
-console.log(checkPrime(5)); 
+// const checkPrime = (num) => {
+//     for(let i = 2; i <= Math.sqrt(num); i++) {
+//         console.log(i);
+//         if (num % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true
+// }
+// console.log(checkPrime(5)); 
 
 
-const printPrimes = (limit) => {
-    for (i = 1; i <= limit; i++) {
-        if (checkPrime(i)) {
-            console.log(i);
-        }
-    }
+// const printPrimes = (limit) => {
+//     for (i = 1; i <= limit; i++) {
+//         if (checkPrime(i)) {
+//             console.log(i);
+//         }
+//     }
     
+// }
+
+// console.log(printPrimes(97));
+
+
+
+ ////////////////////////
+// ROCK PAPER SCISSORS
+////////////////////////
+
+//Array of game moves
+gameMove = ["rock", "paper", "scissors"];
+
+const randomMove = () => {
+    //gets a random integer with a max parameter value
+    const getRandomNum = () => {
+        return Math.floor(Math.random() * (gameMove.length));
+    }
+    // returns the random index of the gameMove array 
+    return gameMove[getRandomNum()];
 }
 
-console.log(printPrimes(97));
+//stores the random move in each of the player variables
+let computersMove = randomMove();
+let playersMove = randomMove();
 
+//function to play the game
+const rockPaperScissors = (computersMove, playersMove) => {
+    //consoles the player & computer moves
+    console.log(`computer chose ${computersMove}`);
+    console.log(`player chose ${playersMove}`);
+    //conditional 
+    if (computersMove === playersMove) {
+        console.log(`${computersMove} ties ${playersMove}, try again!`)
+    } else if (computersMove === "rock" && playersMove === "scissors"|| computersMove === "paper" && playersMove === "rock" || computersMove === "scissors" && playersMove === "paper"){    
+        console.log(`${computersMove} beats ${playersMove}, computer wins!`);
+    } else if(playersMove === "rock" && computersMove === "scissors"|| playersMove === "paper" && computersMove === "rock" || playersMove === "scissors" && computersMove === "paper"){    
+        console.log(`${playersMove} beats ${computersMove}, players wins!`);
+    }
+}
 
+rockPaperScissors(computersMove, playersMove);
