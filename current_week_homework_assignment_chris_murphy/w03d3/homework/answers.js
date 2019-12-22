5ac7550f42dab1f3e5725d3293c10d20c1a66d25
Chris <cl@Chriss-MBP.fios-router.home>
2019-12-22 12:20:33 -0500

    crazy object complete

--- current_week_homework_assignment_chris_murphy/w03d3/homework/answers.js
+++ current_week_homework_assignment_chris_murphy/w03d3/homework/answers.js
@@ -66,7 +66,9 @@
 const colorsOfRainbow = ["red", "orange", "yellow", "green", "blue", "indigo",  "violet"];
 
 // Write code that will access "blue" from the rainbow array.
-console.log(colorsOfRainbow[4]);
+// console.log(colorsOfRainbow[4]);
+
+
 // Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
 
 const chrisMurphy = {
@@ -77,4 +79,56 @@ const chrisMurphy = {
 };
 
 // Write code that will access your hobby from the object that you just created.
-console.log(chrisMurphy.hobby);
+// console.log(chrisMurphy.hobby);
+
+
+
+
+
+
+
+  //////////////////////////
+ ////Crazy Object!
+//////////////////////////
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+// Use crazy Object to log the following.
+
+// "omg my mouth is burning"
+let hottestSalsa = crazyObject.taco[1].salsa[crazyObject.taco[1].salsa.length - 1];
+console.log(hottestSalsa);
+// "Pretty pretty prettayyyyy good"
+console.log(crazyObject.larry.quotes[0]);
+// "Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+// "Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
+// The object the contains the name funkhauser
+console.log(crazyObject.larry.characters[1]);
+// Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
+crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
+console.log(crazyObject.larry.quotes);
