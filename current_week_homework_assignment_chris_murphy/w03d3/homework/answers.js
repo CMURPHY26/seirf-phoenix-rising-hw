3e9ac13ff288cc298d0b417c3b5de389ccf1d3e9
Chris <cl@Chriss-MBP.fios-router.home>
2019-12-22 12:05:31 -0500

    datatype refresher complete

---
+++ current_week_homework_assignment_chris_murphy/w03d3/homework/answers.js
@@ -0,0 +1,50 @@
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// e.g. A computer that can be either on or off. 
+// => datatype: boolean 
+// => data structure example: const computer = true; 
+
+// A light switch that can be either on or off.
+// => datatype: boolean
+// => data structure example: const lightSwitch = {on: true} 
+
+// A user's email address.
+// => datatype: string
+// => data structure example: const user = {emailAddress: "joe@gmail.com"}
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// => datatype: array or object
+// => data structure example: 
+//const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"]
+//const spaceship = {features:["hull", "laser blasters", "tractor beam", "warp drive"]}
+
+// A list of student names from our class.
+// => datatype: array
+// => data structure example: const studentNames = ["personA", "personB"]
+
+
+// A list of student names from our class, each with a location.
+// => datatype: object and array
+// => data structure example: 
+// const class = {
+//     students: [
+//         {name: "Chris", location: "New York"},
+//         {name: "Joe", location: "New York"}
+//     ]
+// }; 
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// => datatype: object and aray
+// => data structure example: 
+// const class = {
+//     students: [
+//         {name: "Chris", 
+//         location: "New York"
+//         favoriteTvShow: ["Breaking Bad", "Parks and Rec", "Sopranos"]
+//         },
+//         {name: "Joe", 
+//         location: "New York"
+//         favoriteTvShow: ["Breaking Bad", "Parks and Rec", "Sopranos"]
+//         }
+//     ]
+// }; 
