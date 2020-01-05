78764b22fe03e2c391db14b49f0b41dc17849fb4
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-05 18:16:29 -0500

    Combine objects, arrays, and functions more than one level deep

--- current_week_homework_assignment_chris_murphy/w04d1/w04d1_homework/combining_datatypes.js
+++ current_week_homework_assignment_chris_murphy/w04d1/w04d1_homework/combining_datatypes.js
@@ -33,3 +33,38 @@ const apartmentBuilding = [["joe", "chris", "tara", "lauren", "alex"],105,{stree
 
 console.log(apartmentBuilding[0][3]);
 
+
+
+  /////////////////////////////////////////////////////////////////////
+ /// Combine objects, arrays, and functions more than one level deep
+//////////////////////////////////////////////////////////////////////
+
+// Create a function knit that returns an object that has the following kinds of properties item: scarf, size : 6ft etc. Log a value of that object (hint: call the function and then call a property on the return value).
+
+const knit = () => {
+    return {item: "scarf", size: "6ft", color: "purple"};
+}
+
+console.log(knit().item);
+
+// Create a function crayonSelector that returns an object that has an array (you can reuse your crayonBox object). Log one of the elements of that array.
+
+// const crayonBox = {
+//     crayons: ["green", "blue", "orange","purple","red","yellow", "violet", "indigo"]
+// };
+const crayonSelector = () => {
+    return crayonBox;
+}
+
+console.log(crayonSelector().crayons[3]);
+
+
+// Create a function powerButton that returns a function called options - options should console.log a simple message like select a song. Call that inner function
+
+const powerButton = () => {
+    return options = () => {
+        console.log("select a song");
+    };
+};
+
+console.log( powerButton()() );
