0e5993f46d56c89d2e892f96cd2a610a6c6f3f73
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-10 19:30:53 -0500

    created our hero dougie

---
+++ current_week_homework_assignment_chris_murphy/w04d3/homework/donut_adventure/adventure.js
@@ -0,0 +1,37 @@
+// console.log("It's working");
+
+// The Adventure of Dougie the Donut on the Streets of NYC.
+
+// Dougie is a funky fresh donut who has decided to walk his way from the Flat Iron District to Times Square where he wants to show off his sweet moves. Along the way, Dougie is approached by his arch nemesis Pizza Rat. Sometimes they fight, sometimes they just talk smack at each other. Anyway that it goes, let's try to get Dougie safely to Times Square!
+
+
+// For this section of the homework, you'll be making two objects that will interact. First we will create a Hero class, then an Enemy class, and instantiate our two objects from those classes.
+
+  ////////////////////
+ //// Our Hero
+///////////////////
+
+class Hero {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            sprinkleSpray: 5,
+            sugarShock: 10
+        }
+        this.catchPhrases = ['i\'m fresher than day old pizza', 
+        'you can\'t count my calories'];
+
+        talkSass () {
+            console.log(this.Math.floor(Math.random() * this.catchPhrases.length));
+        }
+
+        announce () {
+            console.log(this.health);
+        }
+
+        fight () {
+            console.log('I\'m ready to rumble');
+        }
+    }
+}
