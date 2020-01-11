f8dfaed9379e2b9d29adb93544b078009920bf65
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-10 19:09:21 -0500

    created person class

--- current_week_homework_assignment_chris_murphy/w04d3/homework/objects.js
+++ current_week_homework_assignment_chris_murphy/w04d3/homework/objects.js
@@ -17,3 +17,56 @@ class Hamster {
         return this.price;
     }
 }
+
+
+class Person {
+    constructor (name) {
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getName () {
+        return this.name;
+    }
+
+    getAge () {
+        return this.age;
+    }
+
+    getWeight() {
+        return this.weight;
+    }
+
+    greet () {
+        console.log(`Hi ${this.name}! Nice to see you.`);
+    }
+
+    eat () {
+        this.weight += 1;
+        this.mood +=1;
+    }
+
+    exercise () {
+        this.weight -= 1;
+    }
+
+    ageUp () {
+        this.age += 1;
+        this.height += 1;
+        this.weight += 1;
+        this.mood -= 1;
+        this.bankAccount += 10;
+    }
+
+    buyHamster (HamsterObject) {
+        this.hamsters.push(HamsterObject);
+        this.mood += 1;
+        this.bankAccount -= HamsterObject.getPrice();
+    }
+
+}
