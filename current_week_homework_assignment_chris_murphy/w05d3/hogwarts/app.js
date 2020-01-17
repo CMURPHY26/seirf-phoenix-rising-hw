47d59dac6d7c689c00db4f918c43789055a5050f
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-16 20:59:08 -0500

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- current_week_homework_assignment_chris_murphy/w05d3/hogwarts/app.js
+++ current_week_homework_assignment_chris_murphy/w05d3/hogwarts/app.js
@@ -4,7 +4,7 @@ $( () => {
         console.log('oops! I still have to link my jQuery properly!');
       } else {console.log('I did it! I linked jQuery and this js file!')};
       
-
+      //Year 1
       const $container = $(".container");
       
       const $h1 = $("<h1>").text("Hogwarts");
@@ -13,6 +13,7 @@ $( () => {
 
       $container.append($h1);
 
+      //Year 2
       const $h2 = $("<h2>").text("Chris and Lauren");
       const $h3 = $("<h3>").text("Gryffindor");
       const $h4 = $("<h4>").addClass("dog").text("Indi");
@@ -22,7 +23,32 @@ $( () => {
       $container.append($h3);
       $container.append($h4);
       $container.append($h4Wand);
+
+
+      //Year 3
+      const $ul = $("<ul>").attr("storage", "trunk");
       
+
+      const listOfItems = ["butter beer", "invisibility cloak","magic map","time turner", "leash","Bertie Bott's Every Flavor [Jelly] Beans"];
+
+      for(let i = 0; i < listOfItems.length; i++) {
+          let $li = $("<li>").text(listOfItems[i]);
+          $ul.append($li);
+      };
+
+      $container.append($ul);
+      
+      for(let i = 1; i < 4; i ++){
+      $("li").eq(i).addClass("secret");
+      }
+
+      $("li").eq(4).addClass("dog");
+      
+
+
+
+
+
 
 
 
