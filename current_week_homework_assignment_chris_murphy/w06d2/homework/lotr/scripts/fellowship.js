e54a09f27963d8b4a7808d8ea64cac0d96faaf23
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-19 14:43:47 -0500

    Chapter 3 complete - Made the ring and gave it to Frodo

--- current_week_homework_assignment_chris_murphy/w06d2/homework/lotr/scripts/fellowship.js
+++ current_week_homework_assignment_chris_murphy/w06d2/homework/lotr/scripts/fellowship.js
@@ -94,9 +94,10 @@ const makeHobbits = () => {
 const keepItSecretKeepItSafe = () => {
   console.log('keeping secrets');
   // 1. create an empty div with an id of 'the-ring'
-
+    const $ringDIV = $("<div>").attr("id", "the-ring");
   // 2. add the ring as a child of Frodo
-    // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
+  // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
+    $(".hobbit").eq(0).append($ringDIV);
 
     // when you think you have given Frodo the ring, check in your Elements tab to see that it works correctly
 
