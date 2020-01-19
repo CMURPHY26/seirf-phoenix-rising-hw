f4d0f929348605244e781529eb9dcb6f8a4380c5
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-19 14:39:07 -0500

    Chapter 2 complete - Made the Hobbits

--- current_week_homework_assignment_chris_murphy/w06d2/homework/lotr/scripts/fellowship.js
+++ current_week_homework_assignment_chris_murphy/w06d2/homework/lotr/scripts/fellowship.js
@@ -70,16 +70,18 @@ const makeMiddleEarth = () => {
 const makeHobbits = () => {
   console.log('making Hobbits');
   // Goal: display an unordered list of hobbits in the shire (which is the first article tag on the page)
-
   // 1. create a 'ul'
-
+    const $hobbitUL = $("<ul>");
   // 2. make each hobbit an li element and append it to the 'ul' you just created
     // hint: use the given 'hobbits' array and use a for loop
-
-  // 3. also, give each hobbit (`li`) a class of "hobbit"
-
+    hobbits.forEach(element => {
+      // 3. also, give each hobbit (`li`) a class of "hobbit"
+      let $li = $("<li>").addClass("hobbit").text(element);
+      $hobbitUL.append($li);
+    });
   // 4. append the ul to the shire
     // hint: get 'The-Shire' by using its id
+    $("#The-Shire").append($hobbitUL);
 
 };
 
