320c43bb21fd1bd2abfe8d055ac8dc642f30e14b
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-23 18:52:08 -0500

    set up createToDo function with console log message for value of input

--- current_week_homework_assignment_chris_murphy/w06d3/homework/app.js
+++ current_week_homework_assignment_chris_murphy/w06d3/homework/app.js
@@ -1,12 +1,17 @@
 
+const createToDo = () => {
+    $("#submit").on("click", ()=> {
+        console.log($("#input-box").val());
+    })
 
+}
 
 
 
 
 
 $(()=> {
-
-
+    console.log("It's working");
+    createToDo();
 });
 
