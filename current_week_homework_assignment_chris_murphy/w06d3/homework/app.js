dac1f3b2ba2b56d399b2132b5af82685fc88e8bd
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-23 19:26:46 -0500

    Finished logic for createToDo function

--- current_week_homework_assignment_chris_murphy/w06d3/homework/app.js
+++ current_week_homework_assignment_chris_murphy/w06d3/homework/app.js
@@ -3,9 +3,11 @@ const createToDo = () => {
     $("#submit").on("click", ()=> {
         console.log( $("#input-box").val() );
         let $toDoItem = $("#input-box").val();
-        let $div = $("<div>").addClass("to-do-item").text($toDoItem);
+        let $div = $("<div>").addClass("to-do-item")
+        let $p = $("<h3>").text($toDoItem);
+        let $button = $("<button>").addClass("completed-remove").text("COMPLETED");;
+        $div.append($p,$button);
         $("#to-do-list").append($div);
-        
     });
 
 }
