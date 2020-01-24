6b65cc1001d5fd43d5f95cb3ae51546cb923093f
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-23 20:26:39 -0500

    Part Four Complete - user cna click complete and move item to done column

--- current_week_homework_assignment_chris_murphy/w06d3/homework/app.js
+++ current_week_homework_assignment_chris_murphy/w06d3/homework/app.js
@@ -1,23 +1,31 @@
 
 const createToDo = () => {
-    $("#submit").on("click", ()=> {
         console.log( $("#input-box").val() );
         let $toDoItem = $("#input-box").val();
         let $div = $("<div>").addClass("to-do-item")
         let $p = $("<h3>").text($toDoItem);
-        let $button = $("<button>").addClass("completed-remove").text("COMPLETED");;
+        let $button = $("<button>").addClass("completed").text("COMPLETED");
+        $button.on("click", completeToDo);
         $div.append($p,$button);
         $("#to-do-list").append($div);
-    });
+        $("#input-box").val("");
 
 }
 
 
+const completeToDo = (event) => {
+        let $toDo = $(event.currentTarget).parent();
+        $toDo.css('background-color', "#ED6495");
+        $("#completed").append($toDo)
+}
 
 
 
 $(()=> {
     console.log("It's working");
-    createToDo();
+    $("#submit").on("click", createToDo);
+
+       
+    
 });
 
