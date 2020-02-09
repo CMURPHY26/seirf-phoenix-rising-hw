a955c372f8ba73073b69058d9fd8d781724cd0d9
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 11:26:20 -0500

    Tip Calculator express application created.

--- current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
+++ current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
@@ -10,6 +10,7 @@ app.listen(port, () => {
     console.log("server is running on port " + port);
 });
 
+//Greetings
 app.get('/greeting', (req, res)=> {
     res.send("Hello, stranger!");
 });
@@ -17,3 +18,10 @@ app.get('/greeting', (req, res)=> {
 app.get("/greeting/:name", (req, res) => {
     res.send("What's up, " + req.params.name + "? It's great to see you!!");
 });
+
+//Tip Calculator
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    let tip = Number(req.params.total) * (Number(req.params.tipPercentage) / 100);
+    res.send("The tip is: " + tip);
+});
+
