8ecd715c4508b821dfb5533565149b729358a115
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 11:21:31 -0500

    Greeting express application created.

--- current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
+++ current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
@@ -13,3 +13,7 @@ app.listen(port, () => {
 app.get('/greeting', (req, res)=> {
     res.send("Hello, stranger!");
 });
+
+app.get("/greeting/:name", (req, res) => {
+    res.send("What's up, " + req.params.name + "? It's great to see you!!");
+});
