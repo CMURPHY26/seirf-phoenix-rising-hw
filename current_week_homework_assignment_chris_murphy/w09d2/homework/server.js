4ed28a740a7bc72da2e37bddfb56e40ef061131f
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 11:33:25 -0500

    Magic 8 Ball express application created.

--- current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
+++ current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
@@ -25,3 +25,16 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     res.send("The tip is: " + tip);
 });
 
+//Magic 8 Ball
+const m8Ball = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+app.get("/magic/:question", (req, res) => {
+    let randomMessage = m8Ball[(Math.floor(Math.random() * m8Ball.length))];
+    res.send(`<h1>
+                ${req.params.question}
+              </h1>
+              <h1>
+                ${randomMessage};
+              </h1>
+    `);
+});
