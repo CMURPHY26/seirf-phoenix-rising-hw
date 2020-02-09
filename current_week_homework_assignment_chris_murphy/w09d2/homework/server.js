f5306695d72ba9c5059a9e82e8f60c41565f1763
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-09 11:16:00 -0500

    created generic greeting route

--- current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
+++ current_week_homework_assignment_chris_murphy/w09d2/homework/server.js
@@ -8,4 +8,8 @@ let port = process.env.PORT;
 
 app.listen(port, () => {
     console.log("server is running on port " + port);
-})
+});
+
+app.get('/greeting', (req, res)=> {
+    res.send("Hello, stranger!");
+});
