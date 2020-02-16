2189a733ec463bc3dcfdd29c45a02ebfb4daf118
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-16 16:17:27 -0500

    set up server files and database

---
+++ current_week_homework_assignment_chris_murphy/w10d2/homework/budgtr/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT;
+
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log("The server is running on port", port);
+})
+
+
+
