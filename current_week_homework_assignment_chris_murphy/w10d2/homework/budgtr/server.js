ea26e6cd84ebbde1f847a48511abe9f946730693
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-16 16:53:35 -0500

    set up index route

--- current_week_homework_assignment_chris_murphy/w10d2/homework/budgtr/server.js
+++ current_week_homework_assignment_chris_murphy/w10d2/homework/budgtr/server.js
@@ -6,6 +6,13 @@ const app = express();
 
 const port = process.env.PORT;
 
+const Budget = require("./models/budget.js");
+
+app.use(express.static("public"));
+
+app.get("/budgets", (req, res) => {
+    res.render("index.ejs", { budget: Budget});
+});
 
 
 
@@ -14,6 +21,7 @@ const port = process.env.PORT;
 
 app.listen(port, () =>{
     console.log("The server is running on port", port);
+    // console.log(budget)
 })
 
 
