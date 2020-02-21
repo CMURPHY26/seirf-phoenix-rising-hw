4a02249b244e2547185cfb3e3619fb7ada19c1e7
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-20 21:03:43 -0500

    set up index and show routes

---
+++ current_week_homework_assignment_chris_murphy/w10d3/homework/pokedex/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+const pokemon = require("./models/pokemon.js")
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+
+
+
+
+
+
+
+
+//SHOW ROUTE
+app.get("/pokemon/:id", (req,res) => {
+    res.render("show.ejs", {pokemon: pokemon[req.params.id]});
+})
+
+
+
+//INDEX ROUTE
+app.get("/pokemon", (req, res) => {
+    // res.send(pokemon);
+    res.render("index.ejs", {pokemon: pokemon});
+});
+
+//ROOT REDIRECT
+app.get("/", (req, res) => {
+    res.redirect("/pokemon");
+})
+
+
+app.listen(port, () => {
+    console.log("The server is running on port", port);
+})
