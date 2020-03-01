dd9d0e7fb11eb5c606afbd15604d963c8021f915
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-02-29 20:19:30 -0500

    updated css styling

--- current_week_homework_assignment_chris_murphy/w11d3/homework/mongoose_store/controllers/products.js
+++ current_week_homework_assignment_chris_murphy/w11d3/homework/mongoose_store/controllers/products.js
@@ -4,27 +4,30 @@ const router = express.Router();
 
 const Product = require("../models/products.js");
 
+
+const imgArray = ["","","","http://pngimg.com/uploads/guitar/guitar_PNG3342.png","http://pngimg.com/uploads/guitar/guitar_PNG3339.png","http://pngimg.com/uploads/guitar/guitar_PNG3367.png","http://pngimg.com/uploads/guitar/guitar_PNG3371.png"];
+
 //ROUTES
 //SEED DATA ROUTE
 router.get('/seed', async (req, res) => {
     const newProducts =
       [
         {
-          name: 'Beans',
-          description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-          img: 'https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2',
-          price: 5,
-          qty: 99
+          name: 'Guitar 1',
+          description: "Subway tile disrupt vinyl, etsy shaman meggings fingerstache swag. Leggings bicycle rights austin, pickled lomo cornhole plaid gochujang kogi humblebrag franzen pabst four loko. Bicycle rights mlkshk portland gentrify, gochujang 90's selvage. Selvage pug master cleanse biodiesel tilde try-hard bushwick fingerstache activated charcoal beard squid man braid gentrify photo booth. Vinyl post-ironic dreamcatcher iceland.",
+          img: 'http://pngimg.com/uploads/guitar/guitar_PNG3349.png',
+          price: 550,
+          qty: 10
         }, {
-          name: 'Bones',
-          description: 'It\'s just a bag of bones.',
-          img: 'http://bluelips.com/prod_images_large/bones1.jpg',
-          price: 25,
+          name: 'Guitar 2',
+          description: "Tumeric chillwave yuccie coloring book, lyft YOLO vaporware etsy adaptogen. Occupy woke tumeric put a bird on it typewriter waistcoat iPhone air plant forage pour-over fanny pack vice freegan. Helvetica succulents typewriter banh mi tumeric pickled. Keffiyeh health goth irony yuccie pop-up letterpress bushwick tofu pug fashion axe retro hoodie selvage. VHS vaporware next level readymade craft beer. Salvia man braid PBR&B, sriracha dreamcatcher +1 scenester freegan cred literally meggings.",
+          img: 'http://pngimg.com/uploads/guitar/guitar_PNG3346.png',
+          price: 800,
           qty: 0
         }, {
-          name: 'Bins',
-          description: 'A stack of colorful bins for your beans and bones.',
-          img: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg',
+          name: 'Guitar 3',
+          description: "Distillery marfa plaid, williamsburg man braid af locavore salvia hammock XOXO organic fam letterpress deep v. Venmo asymmetrical master cleanse taiyaki. Knausgaard pop-up slow-carb YOLO four loko bicycle rights narwhal leggings migas next level forage. Lyft everyday carry tumeric coloring book. Hella 3 wolf moon venmo, poutine chartreuse craft beer banh mi chia. Readymade cold-pressed freegan brunch street art.",
+          img: 'http://pngimg.com/uploads/guitar/guitar_PNG3340.png',
           price: 7000,
           qty: 1
         }
@@ -56,7 +59,7 @@ router.post("/", (req, res) => {
 //EDIT ROUTE
 router.get("/:id/edit", (req, res) => {
   Product.findById(req.params.id, (err, product) =>{
-    res.render("edit.ejs", {id: req.params.id})
+    res.render("edit.ejs", {id: req.params.id, product: product})
   })
 })
 
