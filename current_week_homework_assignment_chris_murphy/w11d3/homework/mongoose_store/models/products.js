ddb72b931137bcb9fc554b51f87d13643fe58d86
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-03-03 21:47:45 -0500

    added Schema validation

--- current_week_homework_assignment_chris_murphy/w11d3/homework/mongoose_store/models/products.js
+++ current_week_homework_assignment_chris_murphy/w11d3/homework/mongoose_store/models/products.js
@@ -1,11 +1,11 @@
 mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: String,
+    name: {type:String, required: true},
     description: String,
     img: String,
-    price: Number,
-    qty: Number
+    price: {type: Number, min: 0},
+    qty: {type: Number, min: 0},
 });
 
 const Product = mongoose.model('Product', productSchema);
