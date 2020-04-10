a28c96add676d40466b83f1c41922a15b7450796
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-10 19:45:03 -0400

    added some more comments

--- current_week_homework_assignment_chris_murphy/w17d3/homework/ruby_practice/ex1.rb
+++ current_week_homework_assignment_chris_murphy/w17d3/homework/ruby_practice/ex1.rb
@@ -5,3 +5,13 @@ puts "This is fun."
 puts "Yay! Printing."
 #puts "I'd much rather you 'not'."
 #puts 'I "said" do not touch this.'
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
