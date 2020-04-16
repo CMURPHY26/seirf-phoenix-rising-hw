7bf168d957de81b2641faf43862775fd032cb18e
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-16 17:16:26 -0400

    solved Euler problem 1

--- current_week_homework_assignment_chris_murphy/w18d2/homework/student_solution.rb
+++ current_week_homework_assignment_chris_murphy/w18d2/homework/student_solution.rb
@@ -171,3 +171,26 @@
 # p print_item_sums table, book
 
 ############################################################
+
+# # Solving Problems with Ruby
+# # Euler Problem 1
+# # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# # Find the sum of all the multiples of 3 or 5 below 1000.
+
+# def sum_of_mulitples
+#     sum = 0
+#     (1...1000).each do |num|
+#         @sum = 0
+#         if num % 3 == 0 || num % 5 == 0
+#             sum += num
+#         end
+#     end
+    
+#     p sum
+# end
+
+# sum_of_mulitples
+
+
+############################################################
