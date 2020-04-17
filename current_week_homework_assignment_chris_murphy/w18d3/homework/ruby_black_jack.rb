33977bd02b025357e845f2aa8ce03f9f3a0344a6
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-17 17:28:33 -0400

    created human and computer hashes

--- current_week_homework_assignment_chris_murphy/w18d3/homework/ruby_black_jack.rb
+++ current_week_homework_assignment_chris_murphy/w18d3/homework/ruby_black_jack.rb
@@ -1,10 +1,28 @@
-class Human name
+class Human
     attr_accessor :name
     attr_accessor :bankroll
     attr_accessor :hand
-    def initialize
+    def initialize name
         @name = name
         @bankroll = 100
         @hand = []
     end
 end
+
+class Computer 
+    attr_accessor :name
+    attr_accessor :bankroll
+    attr_accessor :hand
+
+    def initialize
+        @name = "the_house"
+        @bankroll = 10000
+        @hand = []
+    end
+end
+
+human = Human.new "Chris"
+p human
+
+the_house = Computer.new
+p the_house
