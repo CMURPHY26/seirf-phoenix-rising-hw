852bb7a24189eb659c3e9b6b1a203f17ccb9ca4d
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-17 18:11:03 -0400

    finished logic to build the deck

--- current_week_homework_assignment_chris_murphy/w18d3/homework/ruby_black_jack.rb
+++ current_week_homework_assignment_chris_murphy/w18d3/homework/ruby_black_jack.rb
@@ -1,12 +1,19 @@
 class Human
+    #add attr_accessor for read and write
+    #for the properties below
     attr_accessor :name
     attr_accessor :bankroll
     attr_accessor :hand
+    #initiantiate a hash of the class by
+    #setting the initalize function in the class
     def initialize name
+        #@variable_name allows the variable to be 
+        #accessible outside this function
         @name = name
         @bankroll = 100
         @hand = []
     end
+
 end
 
 class Computer 
@@ -26,3 +33,44 @@ p human
 
 the_house = Computer.new
 p the_house
+
+deck = []
+suits = ["hearts", "diamonds", "spades", "clubs"]
+class Card
+    attr_reader :value
+    attr_reader :face
+    attr_reader :suit
+
+    def initialize value, face, suit
+        @value = value
+        @face = face
+        @suit = suit
+    end
+end
+
+def create_populate suit
+    deck = []
+    faces = [["Jack", 10],["Queen", 10],["King", 10],["Ace", 11]]
+    (2..10).each do |num| 
+        card = Card.new num, num, suit
+        deck.push(card)
+    end
+
+    faces.each do |face|
+        card = Card.new face[1], face[0], suit
+        deck.push(card)
+    end
+
+    deck
+end
+
+suits.each do |suit|
+deck.push(create_populate suit)
+end
+
+deck = deck.flatten
+p deck
+
+deck = deck.shuffle!
+
+p deck
