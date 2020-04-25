5e4c2eb69bedd094f73ff7350af5da6fb76847f6
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 16:05:11 -0400

    Commit 5: Added routes and controller

--- current_week_homework_assignment_chris_murphy/w20d2/homework/tweeter_app_api/app/controllers/tweets_controller.rb
+++ current_week_homework_assignment_chris_murphy/w20d2/homework/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -1,3 +1,15 @@
 class TweetsController < ApplicationController
 
+    def index
+        tweets = Tweet.all
+        render status: 200, json: {tweets:tweets}
+    end
+
+    def show
+        tweet = Tweet.find(params[:id])
+        render status: 200, json: {tweet: tweet}
+    end
+
+    private
+
 end
