fee4a17c9e7468e161132aeec4b2a656f8193405
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-25 16:01:39 -0400

    Commit 4: Created model and seeded db

--- current_week_homework_assignment_chris_murphy/w20d2/homework/tweeter_app_api/db/seeds.rb
+++ current_week_homework_assignment_chris_murphy/w20d2/homework/tweeter_app_api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do
+    Tweet.create({
+        title: Faker::Hipster.sentence(word_count: 3),
+        content: Faker::Hipster.sentence(word_count: 20),
+        author: Faker::FunnyName.three_word_name
+    })
+end
