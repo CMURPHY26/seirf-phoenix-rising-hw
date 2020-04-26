d5501740d88767e6f0e4a48506a2ddafac182114
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-26 17:25:12 -0400

    Commit 5: Changed table name

--- current_week_homework_assignment_chris_murphy/w19d3/homework/matrix_app_api/db/schema.rb
+++ current_week_homework_assignment_chris_murphy/w19d3/homework/matrix_app_api/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_212144) do
+ActiveRecord::Schema.define(version: 2020_04_26_212422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "characters", force: :cascade do |t|
+  create_table "matrix_characters", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "image_url"
