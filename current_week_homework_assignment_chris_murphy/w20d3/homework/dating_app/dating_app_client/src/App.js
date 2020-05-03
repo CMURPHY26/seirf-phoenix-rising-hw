f943d31bc09226eac3ecfe33e587534f7752dce8
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-05-03 18:11:36 -0400

    Commit 2: Made React App

---
+++ current_week_homework_assignment_chris_murphy/w20d3/homework/dating_app/dating_app_client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Daters from './components/Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <Daters />
+    </div>
+  );
+}
+
+export default App;
---
+++ current_week_homework_assignment_chris_murphy/w20d3/homework/dating_app/dating_app_client/src/components/Hello.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+class Daters extends Component {
+    render() {
+        return (
+            <>
+            <h1>Hello Dating World</h1>
+            </>
+        )
+    }
+}
+
+
+export default Daters
+
+
+
