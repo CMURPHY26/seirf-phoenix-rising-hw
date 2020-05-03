0171422f32aca07e3c99fbf3ce9aa94c03946bdb
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-05-03 18:38:49 -0400

    Commit 3: React module and controller

--- current_week_homework_assignment_chris_murphy/w20d3/homework/dating_app/dating_app_client/src/components/Hello.js
+++ current_week_homework_assignment_chris_murphy/w20d3/homework/dating_app/dating_app_client/src/components/Hello.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react'
 
 
 class Daters extends Component {
+
+    getDaters = () => {
+        fetch("http://localhost:3000/users")
+        .then(response => response.json())
+        .then(jData => console.log(jData))
+        .catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        this.getDaters()
+    }
+
     render() {
         return (
             <>
